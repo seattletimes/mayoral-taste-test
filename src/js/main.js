4ce9ff2a06c90dbc5340c25e67d02b411d5614d6
Vanessa Martinez <vmartinez.contact@gmail.com>
2017-10-10 17:11:19 -0700

    added styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-// require("./lib/social");
+require("./lib/social");
 // require("./lib/ads");
 // var track = require("./lib/tracking");
 
@@ -6,6 +6,7 @@ var $ = require("./lib/qsa");
 var m = require("./lib/dom");
 
 var container = $.one(".quiz-container");
+
 
 var policies = window.policies;
 
@@ -21,8 +22,14 @@ policies.forEach(function(p, i) {
     });
     var label = m("label", {
       "for": `question-${i}-${c}`
-    }, p[c]);
+    }, [
+      m("span", { class: "speaker" }, c),
+      m("span", { class: "speech" }, p[c])
+    ]);
     cElements.push(input, label);
+    input.addEventListener("change", function() { // when it changes it adds the chosen class
+      label.classList.add("chosen");
+    });
   });
   var item = m("div", { class: "policy", "data-question": i }, [
     m("h3", { class: "question" }, p.question),
@@ -33,13 +40,27 @@ policies.forEach(function(p, i) {
 
 var results = $.one(".results-container");
 
+var showSpeaker = function() {
+  console.log(this.candidateName);
+};
+
 var onChange = function() {
   var checked = $("input:checked", container);
   var values = checked.map(el => el.value);
   var counts = { durkan: 0, moon: 0 };
   values.forEach(v => counts[v]++);
   console.log(counts);
-  results.innerHTML = `Your matches: Durkan ${counts.durkan}, Moon ${counts.moon}`;
+  results.innerHTML = `<span class="matchesTitle">Your matches</span>
+
+    <div class="info">
+      <div class="candidateDurkan">Durkan <span class="number">${counts.durkan}</span></div>
+
+      <div class="counter">X/X</div>
+
+      <div class="candidateMoon">Moon <span class="number">${counts.moon}</span></div>
+    </div>
+
+    `;
 }
 
 container.addEventListener("change", onChange);
