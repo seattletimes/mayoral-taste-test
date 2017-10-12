fe7408b7d28b060f8d72fa02fc857cdf16bf9b7c
Vanessa Martinez <vmartinez.contact@gmail.com>
2017-10-12 16:27:03 -0700

    NOs added and templating changed

--- src/js/main.js
+++ src/js/main.js
@@ -4,52 +4,47 @@ require("./lib/social");
 
 var $ = require("./lib/qsa");
 var m = require("./lib/dom");
+var closest = require("./lib/closest");
+var dot = require("./lib/dot");
+var policyTemplate = dot.compile(require("./_policy.html"));
+var ynTemplate = dot.compile(require("./_yesno.html"));
 
 var container = $.one(".quiz-container");
 var submitButton = $.one(".submit");
 
+var durkanBio = $.one(".durkan-bio");
+var moonBio = $.one(".moon-bio");
 
 var policies = window.policies;
 
-policies.forEach(function(p, i) {
-  var candidates = Math.random() > .5 ? ["durkan", "moon"] : ["moon", "durkan"];
-  var cElements = [];
-  candidates.forEach(function(c) {
-    var input = m("input", {
-      type: "radio",
-      name: `question-${i}`,
-      id: `question-${i}-${c}`,
-      value: c
-    });
-    var label = m("label", {
-      "for": `question-${i}-${c}`
-    }, [
-      m("div", { class: "column check-container" }),
-      m("div", { class: "column speech-container" }, [
-        m("div", { class: "speaker" }, c),
-        m("div", { class: "speech" }, p[c])
-      ])
-    ]);
-    cElements.push(input, label);
-    input.addEventListener("change", function() { // when it changes it adds the chosen class
-      item.classList.add("chosen");
-    });
-  });
-  var choices = m("div", { class: "choices"}, cElements);
-  var item = m("div", { class: "policy", "data-question": i }, [
-    m("h3", { class: "question" }, p.question),
-    choices
-  ]);
-  container.appendChild(item);
+var html = "";
+policies.forEach(function(policy, index) {
+  if (policy.yesno) {
+    html += ynTemplate({ policy, index });
+  } else {
+    var candidates = Math.random() > .5 ? ["durkan", "moon"] : ["moon", "durkan"];
+    html += policyTemplate({ policy, index, candidates });
+  }
 });
+container.innerHTML = html;
 
 var results = $.one(".results-container");
 
-var onChange = function() {
+var getScore = function() {
   var checked = $("input:checked", container);
   var values = checked.map(el => el.value);
   var counts = { durkan: 0, moon: 0 };
   values.forEach(v => counts[v]++);
+  counts.total = values.length;
+  return counts;
+};
+
+var onChange = function(e) {
+  if (e) {
+    var item = closest(e.target, ".policy");
+    item.classList.add("chosen");
+  }
+  var counts = getScore();
   console.log(counts);
   results.innerHTML = `
 
@@ -57,14 +52,14 @@ var onChange = function() {
       <div class="matchesTitle">Your matches:</div>
       <div class="candidates">Durkan <span class="number">${counts.durkan}</span>, Moon <span class="number">${counts.moon}</span></div>
       <div class="spacer"></div>
-      <div class="counter">${values.length}/${policies.length}</div>
+      <div class="counter">${counts.total}/${policies.length}</div>
     </div>
 
     `;
-}
+};
 
 var onSubmit = function() {
-  console.log("hi");
+  
 }
 
 container.addEventListener("change", onChange);
