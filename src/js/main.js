cc0c0bb983c50a76cb6f753f2f178c24ddeaa19c
Vanessa Martinez <vmartinez.contact@gmail.com>
2017-10-12 17:36:41 -0700

    working on updating results bar

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,8 @@ var ynTemplate = dot.compile(require("./_yesno.html"));
 var container = $.one(".quiz-container");
 var submitButton = $.one(".submit");
 
+var resultsBio = $.one(".bio-intro");
+var tiedBio = $.one(".bio-tie");
 var durkanBio = $.one(".durkan-bio");
 var moonBio = $.one(".moon-bio");
 
@@ -45,13 +47,11 @@ var onChange = function(e) {
     item.classList.add("chosen");
   }
   var counts = getScore();
-  console.log(counts);
+  // console.log(counts);
   results.innerHTML = `
 
     <div class="info">
-      <div class="matchesTitle">Your matches:</div>
-      <div class="candidates">Durkan <span class="number">${counts.durkan}</span>, Moon <span class="number">${counts.moon}</span></div>
-      <div class="spacer"></div>
+      <div class="candidates"><span class="matchesTitle">Your matches:</span> Durkan <span class="number">${counts.durkan}</span>, Moon <span class="number">${counts.moon}</span></div>
       <div class="counter">${counts.total}/${policies.length}</div>
     </div>
 
@@ -59,7 +59,27 @@ var onChange = function(e) {
 };
 
 var onSubmit = function() {
-  
+  var score = getScore();
+
+  // resets hidden classes
+  resultsBio.classList.add("hidden");
+  tiedBio.classList.add("hidden");
+  durkanBio.classList.add("hidden");
+  moonBio.classList.add("hidden");
+
+
+  // removes hidden classes from results
+  if (score.durkan > score.moon) {
+    resultsBio.classList.remove("hidden");
+    durkanBio.classList.remove("hidden");
+  } else if (score.moon > score.durkan) {
+    resultsBio.classList.remove("hidden");
+    moonBio.classList.remove("hidden");
+  } else { // a tie
+    tiedBio.classList.remove("hidden");
+    durkanBio.classList.remove("hidden");
+    moonBio.classList.remove("hidden");
+  }
 }
 
 container.addEventListener("change", onChange);
